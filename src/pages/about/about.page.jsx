846bcb1529ac5f749274886d31a1437fc9b06cff
James Volmert <james.volmert@gmail.com>
2021-02-28 20:30:36 -0700

    added image to finish out about page

--- src/pages/about/about.page.jsx
+++ src/pages/about/about.page.jsx
@@ -4,8 +4,9 @@ import './about.page.scss';
 
 const AboutPage = () => (
     <section className="about">
+        <img alt="" aria-hidden="true"/>
         <h2>About</h2>
-        <p>Being based at the foot of the Rocky Mountains in Denver Colorado, outdoor influences are apparent in the quality and integrity of our garments. We own and operate our own boutique in Denver which exclusively carries Crown Clothes products and collaborations.</p>
+        <p>Being based at the foot of the Rocky Mountains in Denver Colorado, outdoor influences are apparent in the quality and integrity of our garments. We own and operate our own boutique in Denver which exclusively carries <strong>Crown Clothes</strong> products and collaborations.</p>
         <p> As we have grown in the years since and widened our product range the brand has garnered attention from a larger audience for craftsmanship, attention to detail, and unique design.</p>
         <p>Our branding activities are focused around the unique
 media content showcasing our product. We have pushed professional level photography and video through grassroots efforts that are built around our events/parties and spread through social media channels which has reached hundreds of thousands of advocates.</p>
