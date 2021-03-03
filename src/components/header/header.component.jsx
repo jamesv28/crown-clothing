9c0118436ea78bd729711f1dc2b34655ef1d4545
James Volmert <james.volmert@gmail.com>
2021-03-03 14:37:42 -0700

    added carticon and cart dropdown component and included state for displaying dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,10 +3,12 @@ import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <nav className="header">
         <Link to="/" className="logo-container">
             <Logo className="logo" />
@@ -41,12 +43,17 @@ const Header = ({currentUser}) => (
                     </strong>
                 </Link>
             }
+            <CartIcon />
         </div>
+        { hidden ? null :
+        <CartDropdown />
+        }
     </nav>
 )
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
