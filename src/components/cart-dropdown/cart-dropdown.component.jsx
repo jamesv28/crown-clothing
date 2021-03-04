5d4636487683fbc28ad972e84b2bb14295929819
James Volmert <james.volmert@gmail.com>
2021-03-03 19:38:47 -0700

    updated checkout component with memoize.

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {selectCartItems} from '../../redux/cart/cart.selectors';
 
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
@@ -9,16 +10,19 @@ import './cart-dropdown.styles.scss';
 const CartDropdown = ({cartItems}) => (
     <div className="cart-dropdown">
         <div className="cart-items">
-            { 
-            cartItems.map(cartItem => <CartItem  key={cartItem.id} item={cartItem} />)
+            { cartItems.length ?
+              cartItems.map(cartItem => (
+                <CartItem key={cartItem.id} item={cartItem} />
+              )) : 
+              <span>No items added to cart</span>
             }
         </div>
         <Button>GO TO CHECKOUT</Button>
     </div>
 )
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
   });
 export default connect(mapStateToProps)(CartDropdown);
 
