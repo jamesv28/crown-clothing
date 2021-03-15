94436b3675c7c294058223c8d2f5dd1d0f2717f4
James Volmert <james.volmert@gmail.com>
2021-03-15 13:28:06 -0600

    created higher order component to deal with spinner while loading data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -3,12 +3,20 @@ import {Route} from 'react-router-dom';
 
 import CollectionPage from '../collection/collection.page';
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
 import {updateCollections} from '../../redux/shop/shop.actions';
 import {connect} from 'react-redux';
 import {firestore, convertCollectionSnapshotToMap} from '../../firebase/firebase.utils';
 
+const CollectionOverwithWithSpinner = WithSpinner(CollectionOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends Component{
+
+    state = {
+        loading: true
+    }
 
     unsubscribeFromSnapshot = null;
 
@@ -20,7 +28,9 @@ class ShopPage extends Component{
         this.unsubscribeFromSnapshot = collectionRef.onSnapshot(async snapShot => {
             const collectionsMap = convertCollectionSnapshotToMap(snapShot);
             updateCollections(collectionsMap);
-
+            this.setState({
+                loading: false
+            })
         });
 
     }
@@ -31,11 +41,12 @@ class ShopPage extends Component{
     }
 
     render() {
+        const {loading} = this.state;
         const {match} = this.props;
         return (
             <div className="shop-page">
-                <Route exact path={`${match.path}`} component={CollectionOverview} />
-                <Route path={`${match.path}/:collectionId`}  component={CollectionPage} />
+                <Route exact path={`${match.path}`} render={props => <CollectionOverwithWithSpinner isLoading={loading} {...props} /> }/>
+                <Route path={`${match.path}/:collectionId`}  render={props => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
             </div>
         )
     }
